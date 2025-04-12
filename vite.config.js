da04c7dd21c4628b6137bc12fae729f327d8a44d
Urieu Nicolas <kriskoliba@gmail.com>
2025-04-12 16:14:51 -0400

    fala asim

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 10000 // Definindo o limite para 1000KB (1MB)
+    chunkSizeWarningLimit: 10000, // Definindo o limite para 1000KB (1MB)
+    rollupOptions: {
+      external: ['axios'] // Adicionando axios como dependência externa
+    }
   }
-})
+});
