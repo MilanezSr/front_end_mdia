133275972e3ac27d01d5058686ee6d854c8ba403
Urieu Nicolas <kriskoliba@gmail.com>
2025-04-14 11:24:30 -0400

    Funciona...

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Definindo explicitamente o diretório de saída
-    chunkSizeWarningLimit: 10000, // Definindo o limite para 10000KB (10MB)
+    outDir: 'dist',  // Garantir que a saída seja no diretório 'dist'
+    chunkSizeWarningLimit: 10000,
     rollupOptions: {
-      external: ['axios'] // Caso queira manter o axios como dependência externa
+      external: ['axios']
     }
   }
 });
